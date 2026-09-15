66619c9c3e7ba3f5d9cdd4e12da788d0ea3edbe3
Joe Eichenhofer <jeichenhofer@users.noreply.github.com>
2026-09-14 18:04:39 -0700

    fix install script to avoid recursive linking

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 source zsh/zshenv
 
 mkdir -p "$ZDOTDIR"
-ln -sf "$(pwd)/zsh/zshenv" "$HOME/.zshenv"
-ln -sf "$(pwd)/zsh/zshrc" "$ZDOTDIR/.zshrc"
-ln -sf "$(pwd)/zsh/spaceship" "$ZDOTDIR/spaceship"
-ln -sf "$(pwd)/zsh/spaceship-config.zsh" "$ZDOTDIR/spaceship-config.zsh"
+ln -sfn "$(pwd)/zsh/zshenv" "$HOME/.zshenv"
+ln -sfn "$(pwd)/zsh/zshrc" "$ZDOTDIR/.zshrc"
+ln -sfn "$(pwd)/zsh/spaceship" "$ZDOTDIR/spaceship"
+ln -sfn "$(pwd)/zsh/spaceship-config.zsh" "$ZDOTDIR/spaceship-config.zsh"
